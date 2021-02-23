d83cc881ce7fc6363f4660da9086c20c0f7f99a1
AntonNikonov1 <62539462+AntonNikonov1@users.noreply.github.com>
2021-02-23 20:37:24 +0200

    Feature  product card (#7)
    * Develop (#2)
    
    * add antd & axios dependencies
    
    * card-prototype
    
    * card fixed
    
    * add rating
    
    * fixed
    
    * fixed
    
    * fixed functions
    
    * fixed
    
    * fixed
    
    * fixed
    
    * fixed
    
    * fixed
    
    * fixed
    
    Co-authored-by: Evgeniy Shigartsov <74241715+EvgeniyShigartsov@users.noreply.github.com>
    Co-authored-by: Anton Nikonov <antonanton@MacBook-Pro-Anton.local>

---
+++ src/components/ProductCard/CheckAvailability/CheckAvailability.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import StyledCheckAvailabilityWrapper, { StyledCheckAvailabilityIcon, StyledCheckAvailabilityText } from './StyledCheckAvailability';
+
+export const CheckAvailability = () => (
+  <StyledCheckAvailabilityWrapper>
+    <StyledCheckAvailabilityIcon />
+    <StyledCheckAvailabilityText>check availability</StyledCheckAvailabilityText>
+  </StyledCheckAvailabilityWrapper>
+)
+
+export default CheckAvailability;
