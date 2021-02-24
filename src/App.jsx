59333625d033039c1e2fd72148f6c52a392e2b25
OlhaTitova <63728704+OlhaTitova@users.noreply.github.com>
2021-02-24 13:02:26 +0200

    Feature  cart (#10)
    * create file cart.jsx
    
    * add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import { BannerSlider } from './components/BannerSlider/BannerSlider'
+import Button from './components/Buttons/Button'
 import { NewProductsSlider } from './components/NewProductsSlider/NewProductsSlider'
+import { ProductCard } from './components/ProductCard/ProductCard'
 
 function App() {
   return (
     <div>
+      <ProductCard
+        title="Apple MacBook Air 13 256Gb Space Gray (MWTJ2) 2020"
+        img="https://i.citrus.ua/imgcache/size_800/uploads/shop/0/8/08d983e24e5cced849bd3ab8ac562b35.jpg"
+        lastPrice="544"
+        nowPrice="644"
+        reviews="999"
+        rating={2}
+        isGoodsInStock={false}
+      />
+      <hr />
+      <Button>Submit</Button>
+      <hr />
+      <Button onClick={() => console.log('text')} type="borderBlue">Submit</Button>
+      <hr />
+      <Button type="borderGrey">Submit</Button>
+      <hr />
+      <Button type="black">Submit</Button>
+      <hr />
+      <Button type="yellow">Submit</Button>
+      <hr />
+      <Button size="sm" type="borderBlue">Submit</Button>
+      <hr />
+      <Button size="md" type="black">Submit</Button>
+      <hr />
+      <Button size="lg">Submit</Button>
       <BannerSlider />
       <NewProductsSlider />
     </div>
---
+++ src/components/Buttons/Button.jsx
@@ -0,0 +1,59 @@
+import styled, {css} from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Button = styled.button`
+font-weight: 600;
+font-size: 14px;
+line-height: 21px;
+border-radius: 50px;
+align-items: center;
+width:150px;
+height:50px;
+background-color: #0156FF;
+color: #fff;
+border: 2px solid #0156FF;
+
+${(props) => props.type === 'borderBlue' && css`
+    background-color: #fff;
+    color: #0156FF;
+  `};
+
+  ${(props) => props.type === 'borderGrey' && css`
+    background-color: #fff;
+    color: #666666;
+    border: 2px solid #666666;
+  `};
+
+  ${(props) => props.type === 'black' && css`
+    background-color: #000;
+    border: 2px solid #000;
+  `};
+
+  ${(props) => props.type === 'yellow' && css`
+    background-color: #FFB800;
+    border: 2px solid #FFB800;
+    color: #232C65;
+  `};
+
+  ${(props) => props.size === 'sm' && css`
+    width: 120px;
+    height: 37px;
+  `};
+
+  ${(props) => props.size === 'md' && css`
+    width: 200px;
+    height: 37px;
+  `};
+
+  ${(props) => props.size === 'lg' && css`
+    width: 385px;
+    height: 50px;
+  `};
+`;
+
+export default Button;
+
+Button.propTypes = {
+  type: PropTypes.string.isRequired,
+  size: PropTypes.string.isRequired
+}
