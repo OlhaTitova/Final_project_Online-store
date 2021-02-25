5036828bd1c32f334ccf854fe6bb4a91ffc91bd6
OlhaTitova <63728704+OlhaTitova@users.noreply.github.com>
2021-02-25 11:39:02 +0200

    Feature  cart (#14)
    * create file cart.jsx
    
    * add buttons
    
    * add directiry common
    
    * add CartItem
    
    * add CartItem
    
    * Delete settings.json
    
    Co-authored-by: Evgeniy Shigartsov <74241715+EvgeniyShigartsov@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BannerSlider } from './components/BannerSlider/BannerSlider'
 import Button from './components/common/Buttons/Button'
 import { NewProductsSlider } from './components/NewProductsSlider/NewProductsSlider'
+import {Cart} from './components/Cart/Cart'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Button width={120} height={37} type="borderBlue">Submit</Button>
       <BannerSlider />
       <NewProductsSlider />
+      <Cart />
     </div>
   )
 }
---
+++ src/components/common/Heading/Heading.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Heading = styled.h3`
+color: #000;
+font-weight: 600;
+font-size: 32px;
+line-height: 48px;
+margin: 20px 0;
+`;
+
+export default Heading;
