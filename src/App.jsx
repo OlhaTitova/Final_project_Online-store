149b99e5e7123099168c79837deb3d8e4e7ab0b7
NadiiaStoiko <60553157+NadiiaStoiko@users.noreply.github.com>
2021-02-26 14:57:07 +0200

    About us page (#18)
    * add part of About us section
    
    * add about us page for desctop only
    
    * add changes
    
    * add adaptive of section
    
    * add changes
    
    * add link
    
    * cange app test
    
    * add changes
    
    * Delete settings.json
    
    * change title
    
    Co-authored-by: Evgeniy Shigartsov <74241715+EvgeniyShigartsov@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Route, NavLink } from 'react-router-dom'
 import { BannerSlider } from './components/BannerSlider/BannerSlider'
+import {AboutUsPage} from './pages/About-us/AboutUs'
 import Button from './components/common/Buttons/Button'
 import { NewProductsSlider } from './components/NewProductsSlider/NewProductsSlider'
 import {Cart} from './components/Cart/Cart'
@@ -9,6 +11,12 @@ function App() {
     <div>
       <Button width={120} height={37} type="borderBlue">Submit</Button>
       <BannerSlider />
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/aboutus">About Us</NavLink>
+      <Route exact path="/aboutus">
+        <AboutUsPage />
+      </Route>
+      <Route exact path="/" />
       <NewProductsSlider />
       <Cart />
     </div>
