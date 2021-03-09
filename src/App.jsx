ba4c110f0a2bb635585303ec308fe1ef71a2fab1
Olha Titova <o.titova.ua@gmail.com>
2021-03-09 14:13:20 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
         <NavLink to="/signup">Sign Up</NavLink>
         <NavLink to="/signin">Sign In</NavLink>
         <NavLink to="/aboutus">About Us</NavLink>
-        <NavLink to="/products/720373" style={{ margin: '0 15px', color: 'tomato' }}>Products</NavLink>
+        <NavLink to="/products/739628" style={{ margin: '0 15px', color: 'tomato' }}>Products</NavLink>
         <NavLink to="/buttons">Buttons</NavLink>
       </Container>
       <Switch>
