1bc7e1ada4750119db42bb9b036901786e1dc0b6
Olha Titova <o.titova.ua@gmail.com>
2021-03-15 12:02:40 +0200

    fix

--- src/components/CartWrapper/CartItem/CartItem.jsx
+++ src/components/CartWrapper/CartItem/CartItem.jsx
@@ -21,47 +21,44 @@ export const CartItem = connect(null, {
   decreaseQuantity,
   removeFromCart
 }) => (
-  <div>
-    <StyledCartItem>
-      <Row align="middle">
-        <Col xs={8} md={8} lg={5}>
-          <NavLink to={`products/${product.itemNo}`}>
-            <img src={product.imageUrls[0]} alt={product.name} />
-          </NavLink>
-        </Col>
-        <Col xs={16} md={16} lg={7}>
-          <NavLink to={`/product/${product.itemNo}`}>
-            <p className="bold">{upperCaseFirstLetter(product.name)}</p>
-            <p>{upperCaseFirstLetter(product.description)}</p>
-          </NavLink>
+  <StyledCartItem>
+    <Row align="middle">
+      <Col xs={8} md={8} lg={5}>
+        <NavLink to={`products/${product.itemNo}`}>
+          <img src={product.imageUrls[0]} alt={product.name} />
+        </NavLink>
+      </Col>
+      <Col xs={16} md={16} lg={7}>
+        <NavLink to={`/product/${product.itemNo}`}>
+          <p className="bold">{upperCaseFirstLetter(product.name)}</p>
+          <p>{upperCaseFirstLetter(product.description)}</p>
+        </NavLink>
 
-        </Col>
-        <Col xs={7} md={7} lg={3}>
-          <span className="price">
-            {product.currentPrice}
-            ₴
-          </span>
-        </Col>
-        <Col xs={7} md={7} lg={5}>
-          <DisplayRow>
-            <Button onClick={() => decreaseQuantity(product._id)} shape="circle" icon={<MinusOutlined />} />
-            <StyledInput value={cartQuantity} />
-            <Button onClick={() => increaseQuantity(product._id)} shape="circle" icon={<PlusOutlined />} />
-          </DisplayRow>
-        </Col>
-        <Col xs={7} md={7} lg={3} className="subtotal">
-          <span>
-            {product.currentPrice * cartQuantity}
-            ₴
-          </span>
-        </Col>
-        <Col xs={2} md={3} lg={1}>
-          <ButtonRemoveEdit onClick={() => removeFromCart(product._id)} shape="circle" icon={<CloseOutlined />} />
-        </Col>
-      </Row>
-    </StyledCartItem>
-
-  </div>
+      </Col>
+      <Col xs={7} md={7} lg={3}>
+        <span className="price">
+          {product.currentPrice}
+          ₴
+        </span>
+      </Col>
+      <Col xs={7} md={7} lg={5}>
+        <DisplayRow>
+          <Button onClick={() => decreaseQuantity(product._id)} shape="circle" icon={<MinusOutlined />} />
+          <StyledInput value={cartQuantity} />
+          <Button onClick={() => increaseQuantity(product._id)} shape="circle" icon={<PlusOutlined />} />
+        </DisplayRow>
+      </Col>
+      <Col xs={7} md={7} lg={3} className="subtotal">
+        <span>
+          {product.currentPrice * cartQuantity}
+          ₴
+        </span>
+      </Col>
+      <Col xs={2} md={3} lg={1}>
+        <ButtonRemoveEdit onClick={() => removeFromCart(product._id)} shape="circle" icon={<CloseOutlined />} />
+      </Col>
+    </Row>
+  </StyledCartItem>
 ))
 
 export default CartItem;
