f7849f6bc1d38dc644921d9e2312ce1b790327e5
Evgeniy Shigartsov <74241715+EvgeniyShigartsov@users.noreply.github.com>
2021-04-14 15:28:17 +0300

    tiny fix (#91)

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react'
 import ReactDOM from 'react-dom'
 import App from './App'
