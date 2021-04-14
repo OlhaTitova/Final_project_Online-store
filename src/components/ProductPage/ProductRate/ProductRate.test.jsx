c7d720438dfdef77dc0770c21c4826b264e0fac3
Evgeniy Shigartsov <74241715+EvgeniyShigartsov@users.noreply.github.com>
2021-04-15 00:14:16 +0300

    fix rate comp (#92)

--- src/components/ProductPage/ProductRate/ProductRate.test.jsx
+++ src/components/ProductPage/ProductRate/ProductRate.test.jsx
@@ -4,11 +4,8 @@ import { Provider } from 'react-redux'
 import { render } from '@testing-library/react'
 import ProductRate from './ProductRate'
 import { store } from '../../../store/index'
-import { checkIsCustomerRatedProduct } from './utils/checkIsCustomerRateProduct'
 import { getUpdatedProductTeamplate } from './utils/getUpdatedProductTeamplate'
-import { mockCustomer } from '../../../mocks/customer'
 import { productMock } from '../../../mocks/products'
-import * as customerModule from '../../../store/customer/middleware'
 import * as productsModule from '../../../store/products/middleware'
 
 describe('Product page rate component test', () => {
@@ -46,36 +43,6 @@ describe('Product page rate component test', () => {
       </Provider>
     )
     expect(getAllByRole('radio').length).toBe(5)
-  })
-})
-
-describe('checkIsCustomerRatedProduct util test', () => {
-  const response = {
-    data: mockCustomer,
-    status: 200
-  }
-  test('check is checkIsCustomerRatedProduct util reutrns false with normal script', async () => {
-    customerModule.getCustomer = jest.fn().mockReturnValue(response)
-
-    const result = await checkIsCustomerRatedProduct('123456789')
-    expect(result).toBe(false)
-  })
-
-  test('check is checkIsCustomerRatedProduct util reutrns true if customer rated product before', async () => {
-    customerModule.getCustomer = jest.fn().mockReturnValue(response)
-    
-    const result = await checkIsCustomerRatedProduct(mockCustomer.ratedProducts[0])
-    expect(result).toBe(true)
-  })
-
-  test('check is checkIsCustomerRatedProduct util reutrns null when some troubles on server', async () => {
-    customerModule.getCustomer = jest.fn().mockReturnValue({
-      ...response,
-      status: 500,
-    })
-    
-    const result = await checkIsCustomerRatedProduct(mockCustomer.ratedProducts[0])
-    expect(result).toBe(null)
   })
 })
 
